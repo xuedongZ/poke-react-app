afdf3ff2284ee563fa4178c0363d0015e3d7987a
xuedongZ <zhang.xuedong@infoloop.cn>
2023-12-05 19:57:59 +0800

    initial list

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { LIST_BASE_URL } from './const'
+import { Card } from 'antd'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([])
+
+  const fetchBathList = async () => {
+    try {
+
+      let response = await fetch(LIST_BASE_URL)
+      let outerList = await response.json()
+      outerList = outerList.results
+      console.log(outerList, 'outerList');
+
+      let innerLst = await Promise.allSettled(outerList.map((async item => {
+        let res1 = await fetch(item.url)
+        console.log(res1, 'res1');
+        let res1data = await res1.json()
+        return res1data
+      })))
+
+      innerLst = innerLst.map(item => item.value)
+
+      console.log(innerLst, 'innerLst-zz');
+
+
+      const myUIdata = outerList.map((item, index) => {
+        return {
+          name: item?.name,
+          order: innerLst[index]?.order,
+          id: innerLst[index]?.id,
+          image: innerLst[index]?.sprites?.back_default
+        }
+      })
+      setList(myUIdata);
+
+    } catch (error) {
+      console.log(error, 'zz-err');
+
+    }
+
+  }
+
+  useEffect(() => {
+    fetchBathList()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='card-group-wraper'>
+        {list && list.length && list.map((item) => {
+          return (
+            <Card key={item.id} style={{ width: 300 }}>
+              <p><img src={item.image} /></p>
+            </Card>
+          )
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
